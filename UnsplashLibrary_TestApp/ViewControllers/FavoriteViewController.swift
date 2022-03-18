ec3d8344f86a839c42939367a8fdde014fd0d08e
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-03-18 21:02:51 +0300

    favVC inited

---
+++ UnsplashLibrary_TestApp/ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteViewController.swift
+//  UnsplashLibrary_TestApp
+//
+//  Created by Nechaev Sergey  on 18.03.2022.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+
+    }
+
+}
