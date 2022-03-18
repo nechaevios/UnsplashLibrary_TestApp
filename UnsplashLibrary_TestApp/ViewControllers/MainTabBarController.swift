b3410e44f00b7d00250dd7d3e2e5b2789ca7f91b
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-03-18 21:03:39 +0300

    main tab bar vc prepared

--- UnsplashLibrary_TestApp/ViewControllers/MainTabBarController.swift
+++ UnsplashLibrary_TestApp/ViewControllers/MainTabBarController.swift
@@ -11,7 +11,30 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupTabBarControllers()
 
-        view.backgroundColor = .systemGray
+    }
+
+    private func setupUI() {
+        tabBar.barTintColor = .blue
+        tabBar.backgroundColor = .white
+    }
+
+    private func setupTabBarControllers() {
+        let photosVC = PhotosCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        let favoriteVC = FavoriteViewController()
+
+        viewControllers = [
+            generateNavigationController(rootVC: photosVC, title: "Photos", imageName: "photo"),
+            generateNavigationController(rootVC: favoriteVC, title: "Favorites", imageName: "suit.heart.fill")
+        ]
+    }
+
+    private func generateNavigationController(rootVC: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootVC)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = UIImage(systemName: imageName)
+        return navigationVC
     }
 }
